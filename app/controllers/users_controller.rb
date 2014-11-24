6bc8c64566d26c139ba781b862d9af5965850066
Darren Conley <dcprime@gmail.com>
2014-11-24 18:23:09 +0000

    adding stripe wrapper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,15 +9,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      stripe_payment
+      token = params[:stripeToken]
+      charge = StripeWrapper::Charge.create(:amount => 999, :card => token, :description => "membership fee for #{@user.email}")
       invitation = Invitation.find_by(friend_email: @user.email)
       if invitation
         Relationship.create(follower_id: invitation.user_id, following_id: @user.id)
         Relationship.create(follower_id: @user.id, following_id: invitation.user_id)
       end
-      UserMailer.welcome_email(@user).deliver
-      flash[:notice] = "You have successfully registered"
-      redirect_to sign_in_path
+      if charge.successful?
+        UserMailer.welcome_email(@user).deliver
+        flash[:notice] = "You have successfully registered"
+        redirect_to sign_in_path
+      else
+        flash[:error] = charge.error_message
+        redirect_to register_path
+      end
     else
       flash[:error] = "Please correct the fields below. Your card has not been charged."
       render :new
@@ -58,21 +64,6 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:email, :password, :full_name)
-  end
-  
-  def stripe_payment
-    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-    token = params[:stripeToken]
-    begin
-      charge = Stripe::Charge.create(
-        :amount => 999,
-        :currency => "usd",
-        :card => token,
-        :description => "membership fee for #{@user.email}"
-      )
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-    end
   end
   
 end
